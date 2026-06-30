a05a5def0d644c12ef2be815c09bc4d810251d10
Nisha <nishakamboj1605@gmail.com>
2026-06-30 17:17:17 +0530

    Update CorsConfig.java

--- Backend/src/main/java/com/Nisha/config/CorsConfig.java
+++ Backend/src/main/java/com/Nisha/config/CorsConfig.java
@@ -18,8 +18,12 @@ public class CorsConfig {
 
                 registry.addMapping("/**")
                         .allowedOrigins(
-                                "http://127.0.0.1:5500",
-                                "http://localhost:5500"
+                              
+                                .allowedOrigins(
+    "https://movie-recommendation-and-analysis-p.vercel.app",
+    "http://localhost:5500",
+    "http://127.0.0.1:5500"
+
                         )
                         .allowedMethods("*")
                         .allowedHeaders("*")
